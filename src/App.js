45826624a3e2b01ae59e75171b3ead3ec259f68c
poohbd <p.poohbordee@hotmail.com>
2021-11-10 01:54:11 +0700

    new link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import OnlineComponent from "./components/OnlineComponent"
-import { io } from "socket.io-client";
 
 function App() {
-  const resetinSocket = () =>{
-    console.log("click")
-    const socket = io('http://localhost:8090');
-    socket.emit("resetGame")
+  function resetinSocket(){
+    const url = new URL('http://localhost:3000/reset')
+    fetch(url.href, { method: 'POST' })
   }
 
   return (
@@ -14,7 +12,7 @@ function App() {
       <header className="App-header">
         <h1>Dupme Server</h1>
         <OnlineComponent/>
-        <button className="btn" onClick={()=>resetinSocket()}>Reset</button>
+        <button className="btn" onClick={resetinSocket}>Reset</button>
       </header>
     </div>
   );
